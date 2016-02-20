06832171c71efe84c966d757bebe7f0d90adf6f1
AhyoungRyu <fbdkdud93@hanmail.net>
2016-02-20 22:25:06 +0900

    Deployed b1fd3b8 with MkDocs version: 0.15.2

--- js/extra.js
+++ js/extra.js
@@ -1,19 +1,19 @@
-var original_url = 'http://nflabs.github.io/zeppelinhub-documentation/';
-var new_url = 'http://help.zeppelinhub.com/';
-
-var pagenames = [
-  "",
-  "overview",
-  "zeppelinhub_account",
-  "zeppelin_integration",
-  "sharing_notebooks",
-  "copying_notebooks",
-  "zeppelinhub_viewer",
-  "zeppelin_multitenancy",
-  "support"
-  ];
-
 for ( i = 0; i < pagenames.length; i++ ) {
+  var original_url = 'http://nflabs.github.io/zeppelinhub-documentation/';
+  var new_url = 'http://help.zeppelinhub.com/';
+
+  var pagenames = [
+    "",
+    "overview",
+    "zeppelinhub_account",
+    "zeppelin_integration",
+    "sharing_notebooks",
+    "copying_notebooks",
+    "zeppelinhub_viewer",
+    "zeppelin_multitenancy",
+    "support"
+    ];
+    
   if ( window.location == original_url + pagenames[i]) {
     window.location.href = new_url + pagenames[i];
   }
