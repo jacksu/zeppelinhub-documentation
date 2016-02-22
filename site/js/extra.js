1f12a261c73b83cd92cd377b03757f0fd859dd4c
AhyoungRyu <fbdkdud93@hanmail.net>
2016-02-22 11:47:15 +0900

    Build Success

--- site/js/extra.js
+++ site/js/extra.js
@@ -1,3 +1,21 @@
-if ( window.location == 'http://nflabs.github.io/zeppelinhub-documentation/' ) {
-  window.location.href='http://help.zeppelinhub.com/';
+var pagenames = [
+  "",
+  "overview",
+  "zeppelinhub_account",
+  "zeppelin_integration",
+  "sharing_notebooks",
+  "copying_notebooks",
+  "zeppelinhub_viewer",
+  "zeppelin_multitenancy",
+  "support"
+  ];
+  
+for ( i = 0; i < pagenames.length; i++ ) {
+  var originalUrl = 'http://nflabs.github.io/zeppelinhub-documentation/';
+  var newUrl = 'http://help.zeppelinhub.com/';
+
+  if ( window.location == originalUrl + pagenames[i]) {
+    window.location.href = newUrl + pagenames[i];
+  }
 }
+
